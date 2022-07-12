d0c9cc7a79d0dab52ca56c9c8a0ddab2940318d7
Stein Carsten Sinke <11280647+scsinke@users.noreply.github.com>
2022-07-12 18:30:20 +0200

    Add prettier

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4">
     <p>Custom Layout</p>
-    <hr/>
+    <hr />
     <slot />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-    '@unlighthouse/nuxt',
-  ],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@unlighthouse/nuxt"],
   ssr: false,
   content: {
-    documentDriven: true
+    documentDriven: true,
   },
   typescript: {
     shim: false,
   },
   unlighthouse: {
     scanner: {
-      device: 'desktop',
+      device: "desktop",
     },
   },
-})
+});
