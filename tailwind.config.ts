74980ee6a5794e0c0ac622172a1b27fa5aa7392f
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-07-17 12:36:44 +0200

    Added Location List

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   plugins: [require("daisyui")],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+    }
+  }
 };
