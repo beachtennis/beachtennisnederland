6d799a5cf9353fdb91d565d0a74d3ff75ad604ef
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-07-26 15:25:45 +0200

    added daisyUI config options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,16 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   plugins: [require("daisyui")],
-  theme: {
-    fontFamily: {
-      'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
-    }
-  }
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 };
