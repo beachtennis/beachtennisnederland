b2dbfd8b13a25808b2aea968a69fc8b44a57d844
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-07-28 15:28:15 +0200

    Improved responsive design

--- components/content/homepage/LocationList.vue
+++ components/content/homepage/LocationList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="min-h-screen" id="play">
+  <div class="min-h-screen md:max-w-3xl lg:max-w-6xl m-auto" id="play">
     <h1 class="font-semibold text-3xl px-10 py-10">Locaties</h1>
     <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 mb-10"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-16 mb-10"
     >
       <ContentList
         :query="{ where: { _path: { $ne: '/locaties' } } }"
