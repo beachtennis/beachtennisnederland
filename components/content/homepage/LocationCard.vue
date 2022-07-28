cd5d60afa3a10bce17d3d5d91cd38b125a76ff8f
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-07-28 13:14:12 +0200

    Added links from card to document

--- components/content/homepage/LocationCard.vue
+++ components/content/homepage/LocationCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="card w-80 min-w-80 bg-base-100 dark:bg-base-300 shadow-xl hover:bg-base-200 focus-within:bg-base-200 cursor-pointer">
+      <NuxtLink :to="to">
       <figure>
         <img src="https://placeimg.com/400/225/arch" />
       </figure>
@@ -21,12 +22,13 @@
           </div>
         </div>
       </div>
+      </NuxtLink>
   </div>
 </template>
 
 <script>
 export default {
   name: "LocationCard",
-  props: ["location"],
+  props: ["location", "to"],
 };
 </script>
--- components/content/homepage/LocationList.vue
+++ components/content/homepage/LocationList.vue
@@ -2,19 +2,26 @@
   <div class="min-h-screen" id="play">
     <h1 class="font-semibold text-3xl px-10 py-10">Locaties</h1>
     <div
-      class="x-10 y-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 mb-10"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 mb-10"
     >
-      <LocationCard
-        v-for="location in locations"
-        :key="location.name"
-        :location="location"
-        class="card w-80 min-w-80 bg-base-100 shadow-xl"
-      />
+      <ContentList
+        :query="{ where: { _path: { $ne: '/locaties' } } }"
+        path="/locaties"
+        v-slot="{ list }"
+      >
+        <LocationCard
+          v-for="location in list"
+          :key="location._path"
+          :location="{
+            name: location.name,
+            description: location.description,
+            tag: location.tag,
+            badges: location.badges,
+          }"
+          :to="location._path"
+        >
+        </LocationCard>
+      </ContentList>
     </div>
   </div>
 </template>
-
-<script setup>
-const { data } = await useAsyncData('locations', () => $fetch('/api/locations'))
-const locations = JSON.parse(data.value);
-</script>
