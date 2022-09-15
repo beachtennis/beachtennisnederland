b3663e9f720e622bd1a7586ec6482c9dab84d765
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-15 15:49:41 +0200

    bugfix context

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Oops. Document not found</h1>
+  <br><br>
+    <p>No seriously, can't seem to find what you're looking for...</p>
+  </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@unlighthouse/nuxt"],
+  modules: [
+      "@nuxt/content", 
+      "@nuxtjs/tailwindcss"
+    ],
   ssr: false,
+  target: 'static',
   content: {
     documentDriven: true,
     highlight: {
@@ -12,10 +16,5 @@ export default defineNuxtConfig({
   },
   typescript: {
     shim: false,
-  },
-  unlighthouse: {
-    scanner: {
-      device: "desktop",
-    },
   },
 });
