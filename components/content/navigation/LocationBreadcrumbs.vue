46d14c4f6a06f67478f824b6d42ca7504e7342eb
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-17 00:06:35 +0200

    Added location breadcrumb navigation component

---
+++ components/content/navigation/LocationBreadcrumbs.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-sm breadcrumbs m-2">
+  <ul>
+    <li><NuxtLink to="/">Home</NuxtLink></li> 
+    <li><NuxtLink to="/locations">Locaties</NuxtLink></li> 
+    <li><slot/></li>
+  </ul>
+</div>
+</template>
