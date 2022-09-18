3105482e778fc2bd456b194c07b223ec9b4e30c8
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-18 20:46:28 +0200

    Footer with back button to be rendered in location pages

---
+++ components/content/homepage/footer/FooterWithButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="hero bg-base-200">
+      <div class="container m-auto p-10 md:px-40">
+          <BackHomeButton/>
+        </div>
+    </div>
+    <FooterBar/>
+  </template>
