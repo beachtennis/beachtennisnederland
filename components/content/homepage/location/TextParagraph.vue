871dc04d6653f107b91d86e33ece14a4c5e1d978
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-18 20:46:42 +0200

    Text paragraph to be rendered in location pages

---
+++ components/content/homepage/location/TextParagraph.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-200">
+    <div class="container m-auto">
+        <h1 v-if="header" class="font-semibold text-2xl p-4 px-10 md:px-40">{{header}}</h1>
+        <div class="text-m p-3 px-10 md:px-40">
+          <slot/>
+        </div>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "LocationContent",
+    props: ["header", "text"],
+}
+</script>
