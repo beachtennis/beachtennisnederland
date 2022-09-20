fb574138b255a89a02a565069fbcc9df30689cea
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-20 20:34:43 +0200

    Removed duplicate components

--- components/content/FooterBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
-  <div>
-    <p>Copyright © 2022 - All right reserved by Beach Tennis Nederland.</p>
-  </div>
-</footer>
-</template>
