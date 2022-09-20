a55d5e1e3f9aae8d6d7f80a26656d80194f5b9dd
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2022-09-20 20:54:32 +0200

    Fancy Document Not Found

--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Oops. Document not found</h1>
-  <br><br>
-    <p>No seriously, can't seem to find what you're looking for...</p>
-  </template>
+  <div class="hero">
+    <div class="container">
+      <h1 class="text-xl text-red-800">Oops. Document not found</h1>
+      <p class="text-sm">No seriously, can't seem to find what you're looking for...</p>
+    </div>
+  </div>
+</template>
